1599710448d8b40e304fb73283f78ea729f47332
Bradon Nichols <nichols.bradon@gmail.com>
2021-02-10 09:55:33 -0700

    Lab

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import {HashRouter, Link} from 'react-router-dom'
+import routes from './routes'
 
 export default class App extends Component {
   render() {
     return (
+    <HashRouter>
       <div>
         <nav className='nav'>
           <div>WestSide University</div> 
           <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
+              <link to='/' className='links'>Home</link>
+              <link to='/about' className='links'>About</link> 
           </div>
         </nav>
+        {routes}
       </div>
+    </HashRouter>
     )
   }
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import {link} from 'react-router-dom'
 
 export default class Home extends Component {
   render() {
     return (
       <div className="box">
-        <button className='btn'>Math 1010</button>
-        <button className='btn'>English 2010</button>
-        <button className='btn'>Biology 2020</button>
+        <link to='classlist/MATH1010'><button className='btn'>Math 1010</button></link>
+        <link to='classlist/ENG2010'><button className='btn'>English 2010</button></link>
+        <link to='classlist/BIO2020'><button className='btn'>Biology 2020</button></link>
       </div>
     );
   }
